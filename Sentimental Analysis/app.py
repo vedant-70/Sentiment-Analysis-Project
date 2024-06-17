7fd6ca281cd04ba3bea2f8e02596837b68be6200
vedant-70 <vedant.dubey.aiml22@ggits.net>
2024-06-17 15:31:22 +0530

    Update app.py

--- Sentimental Analysis/app.py
+++ Sentimental Analysis/app.py
@@ -19,4 +19,4 @@ def analyze():
     return render_template('result.html', text=text, sentiment=sentiment_scores)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
